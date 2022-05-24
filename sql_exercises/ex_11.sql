25c80eb9d4b5cac79c900b50af2f2cbe1e177608
Alikhan <59559414+mazhikenalikhan@users.noreply.github.com>
2022-05-24 16:13:50 +0600

    Add files via upload

--- sql_exercises/ex_11.sql
+++ sql_exercises/ex_11.sql
@@ -1,1 +1,1 @@
-SELECT i.InvoiceId, t.Name FROM InvoiceLine i JOIN Track t ON i.TrackId == t.TrackId GROUP BY t.Name;
+SELECT i.InvoiceId, t.Name FROM InvoiceLine i JOIN Track t ON i.TrackId == t.TrackId;
--- sql_exercises/ex_12.sql
+++ sql_exercises/ex_12.sql
@@ -1,1 +1,1 @@
-SELECT i.InvoiceId, t.Name, ar.name FROM InvoiceLine i JOIN Track t ON i.TrackId == t.TrackId JOIN Album al ON al.AlbumId == t.AlbumId JOIN Artist ar ON al.ArtistId == ar.ArtistId GROUP BY t.Name;
+SELECT i.InvoiceId, t.Name, ar.name FROM InvoiceLine i JOIN Track t ON i.TrackId == t.TrackId JOIN Album al ON al.AlbumId == t.AlbumId JOIN Artist ar ON al.ArtistId == ar.ArtistId;
