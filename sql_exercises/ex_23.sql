c0b33fa427a40ff0d145924b4d1e50cb1bd162a1
Alikhan <59559414+mazhikenalikhan@users.noreply.github.com>
2022-05-26 10:38:17 +0600

    Add files via upload

--- sql_exercises/ex_23.sql
+++ sql_exercises/ex_23.sql
@@ -1,2 +1,2 @@
-SELECT t.Name, SUM(il.Quantity) as SUM FROM Track t JOIN InvoiceLine il ON t.TrackId == il.TrackId JOIN Invoice i ON il.InvoiceId == i.InvoiceId WHERE strftime("%Y", i.InvoiceDate) == "2013" GROUP BY t.TrackId ORDER BY SUM DESC; 
+SELECT t.Name, SUM(il.Quantity) as SUM FROM Track t JOIN InvoiceLine il ON t.TrackId == il.TrackId JOIN Invoice i ON il.InvoiceId == i.InvoiceId WHERE strftime("%Y", i.InvoiceDate) == "2013" GROUP BY t.Name ORDER BY SUM DESC; 
 
--- sql_exercises/ex_24.sql
+++ sql_exercises/ex_24.sql
@@ -1,1 +1,1 @@
-SELECT t.Name, SUM(il.Quantity) as SUM FROM Track t JOIN InvoiceLine il ON t.TrackId == il.TrackId JOIN Invoice i ON il.InvoiceId == i.InvoiceId GROUP BY t.TrackId ORDER BY SUM DESC LIMIT 5; 
+SELECT t.Name, SUM(il.Quantity) as SUM FROM Track t JOIN InvoiceLine il ON t.TrackId == il.TrackId JOIN Invoice i ON il.InvoiceId == i.InvoiceId GROUP BY t.Name ORDER BY SUM DESC LIMIT 5; 
